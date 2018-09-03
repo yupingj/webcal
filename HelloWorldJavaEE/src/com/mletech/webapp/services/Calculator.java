c27bbfff3ad72d8be2e2d31c485ac233c5d426ba
Yuping <yupingj@hotmail.com>
2018-09-02 21:57:16 -0400

    add minus in client

--- HelloWorldJavaEE/src/com/mletech/webapp/services/Calculator.java
+++ HelloWorldJavaEE/src/com/mletech/webapp/services/Calculator.java
@@ -21,7 +21,13 @@ public class Calculator {
 	}
 
 	public int minus(int a, int b) {
-		return a - b;
+		Client client = ClientBuilder.newClient();
+		  WebTarget target = client.target(urlString);	
+		  		  
+		  String result = target.path("/minus/"+String.valueOf(a)+"/"+String.valueOf(b)).request(MediaType.TEXT_PLAIN).get(String.class);
+		
+		return Integer.parseInt(result);
+		
 	}
 
 	public int times(int a, int b) {
