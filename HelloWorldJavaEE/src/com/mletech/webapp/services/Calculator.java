0bcad2eae8178f63a469908b3645bb9063c11489
Yuping <yupingj@hotmail.com>
2018-09-02 20:50:11 -0400

    update

--- HelloWorldJavaEE/src/com/mletech/webapp/services/Calculator.java
+++ HelloWorldJavaEE/src/com/mletech/webapp/services/Calculator.java
@@ -1,9 +1,24 @@
 package com.mletech.webapp.services;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
 public class Calculator {
+	
+	Client client = ClientBuilder.newClient();
+	String urlString = "http://localhost:9080/com.mltech.webservice/rest/webcal/";
 
 	public int add(int a, int b) {
-		return a + b;
+		
+		 Client client = ClientBuilder.newClient();
+		  WebTarget target = client.target(urlString);	
+		 
+		  
+		  String result = target.path("/add/"+String.valueOf(a)+"/"+String.valueOf(b)).request(MediaType.TEXT_PLAIN).get(String.class);
+		
+		return Integer.parseInt(result);
 	}
 
 	public int minus(int a, int b) {
